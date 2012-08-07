4c5917981538b2b41b8997e88584eadbf0ec022b
Kate Murphy <kate@kategeek.com>
2012-08-07 02:13:56 -0400

    Jobs section, can add and view jobs. Jobs still need enforces drops.

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,9 @@
 class Job < ActiveRecord::Base
-  attr_accessible :notes, :user
+  attr_accessible :notes, :user, :client_id, :user_id
 
   belongs_to :client
   belongs_to :user
   has_many :drops
+
+  validates :client_id, :presence => true
 end

4c97419edea1a26d80afc7f8e0d26a473cc95f46
Kate Murphy <kate@kategeek.com>
2012-08-07 01:35:46 -0400

    New test users with different roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,19 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'Test Account', :email => 'test@example.com', :password => 'password', :password_confirmation => 'password'
+user = User.create! :name => 'Test Admin', :email => 'admin@example.com', :password => 'password', :password_confirmation => 'password'
 puts 'New user created: ' << user.name
 user.add_role :admin
 
+user = User.create! :name => 'Test Dispatcher', :email => 'dispatcher@example.com', :password => 'password', :password_confirmation => 'password'
+puts 'New user created: ' << user.name
+user.add_role :dispatcher
+user.add_role :courier
+
+user = User.create! :name => 'Test Courier', :email => 'courier@example.com', :password => 'password', :password_confirmation => 'password'
+puts 'New user created: ' << user.name
+user.add_role :courier
+
+user = User.create! :name => 'Test Client', :email => 'client@example.com', :password => 'password', :password_confirmation => 'password'
+puts 'New user created: ' << user.name
+user.add_role :client

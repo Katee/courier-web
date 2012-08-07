b43b2920d87e537b08483b59cd4e61cf76b8ae3a
Kate Murphy <kate@kategeek.com>
2012-08-07 01:59:42 -0400

    More succinct location_update json response

--- app/controllers/location_update_controller.rb
+++ app/controllers/location_update_controller.rb
@@ -1,6 +1,8 @@
 class LocationUpdateController < ApplicationController
   def index
-    @locations = LocationUpdate.find(:all).group_by(&:courier_id)
+    @locations = LocationUpdate.find(:all).group_by(&:courier_id).collect do |courier_id, location_updates|
+      location_updates.collect { |i| {:lat => i.lat, :lng => i.lng} }
+    end
     
     respond_to do |format|
       format.json do

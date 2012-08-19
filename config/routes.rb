4c30ad5ef5460c53b4f79bb59facc6545471c4a1
= <=>
2012-08-19 05:52:56 +0000

    Removed routes logic. Had to hardcode root_path

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,8 @@ Courier::Application.routes.draw do
   resources :locations
   resources :drops
 
-  authenticated :user do
-    root :to => 'home#index'
-  end
+  get "/" => "home#index"
 
-  root :to => "home#index"
   devise_for :users
   resources :users, :only => [:show, :index]
 end

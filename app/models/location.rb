fbe04c6b2a206e3d9de2c4567f6d06e9d803f4af
Katee <hello@kate.io>
2012-09-16 01:59:40 -0400

    Reordered geocode query (fixing an issue where many points incorrectly showed up on Ontario Street) Geocoding will skip if the address doesn't have a number

--- app/models/location.rb
+++ app/models/location.rb
@@ -12,11 +12,15 @@ class Location < ActiveRecord::Base
     }.merge(attributes))
   end
 
+  def full_address
+    [address, postal, city, country, state].reject{|w|w.blank?}.join(',')
+  end
   # Geocode an address with geocoder.ca, costs 1/4 a cent per successful request
   def geocode
-    return if address.nil?
+    return if address.index(/[1-9]/).nil?
+    return if full_address.nil?
     return if not lat.nil? and not lng.nil?
-    url = "http://geocoder.ca/?locate=#{CGI.escape [address, postal, state, city, country].join(',')}&geoit=xml&jsonp=1&auth=250811195292556406039x1903&callback=?"
+    url = "http://geocoder.ca/?locate=#{CGI.escape self.full_address}&geoit=xml&jsonp=1&auth=250811195292556406039x1903&callback=?"
     # get the response and cut off the jsonp
     response = Curl.get(url).body_str.from(2).to(-3)
     json = JSON.parse response
@@ -24,3 +28,4 @@ class Location < ActiveRecord::Base
     self.lat = json["latt"]
   end
 end
+

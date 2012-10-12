7b359fca85507a04375db04badac96b094369830
Katee <hello@kate.io>
2012-10-11 21:40:43 -0400

    Drop factory randomly picks a location_id or creates a location if there are none

--- test/factories/drops.rb
+++ test/factories/drops.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :drop do
-    location_id 1
+    # find a random locatation or create one if there are none
+    location_id { Location.all.sort_by{ rand }.first.id || FactoryGirl.create(:location).id }
+
+    # drops are usually created as part of making a job
     job_id 1
   end
 end

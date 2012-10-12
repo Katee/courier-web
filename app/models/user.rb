002fd70e5a6ac0507797106e67aef78989215bd6
Katee <hello@kate.io>
2012-10-11 21:46:00 -0400

    Adds client_id to User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :client_id
 
   has_many :jobs
+  belongs_to :client
 end
